21ccc79af66154e71bb802f7505ec562d011c92b
Kemp, Jonathan <jkemp@scrippsnetworks.com>
2016-08-09 09:12:47 -0400

    Add boilerplate to run in node for test.

--- tabs.js
+++ tabs.js
@@ -2,29 +2,52 @@
 
 'use strict';
 
-function openJIRAinNewTabs() {
-  var urlPrefix = 'https://sni-digital.atlassian.net/browse/',
-    input = window.prompt('Please enter the JIRA ticket number or numbers:'),
-    value;
+(function(factory) {
+  if (typeof module === 'object' && typeof module.exports === 'object') {
+    var sinon = require('sinon');
 
-  function newTab(path) {
-    var validPath = path.trim();
+    global.chrome = {
+      browserAction: {
+        onClicked: {
+          addListener: sinon.stub()
+        }
+      },
+      tabs: {
+        create: sinon.stub()
+      }
+    };
 
-    chrome.tabs.create({url: urlPrefix + validPath});
-  }
-
-  if (/,/.test(input)) {
-    value = input.split(',');
+    factory(global, global.chrome);
   } else {
-    value = input;
+    factory(window, window.chrome);
+  }
+}(function(window, chrome) {
+
+  var openJIRAinNewTabs = window.openJIRAinNewTabs = function() {
+    var urlPrefix = 'https://sni-digital.atlassian.net/browse/',
+      input = window.prompt('Please enter the JIRA ticket number or numbers:'),
+      value;
+
+    function newTab(path) {
+      var validPath = path.trim();
+
+      chrome.tabs.create({url: urlPrefix + validPath});
+    }
+
+    if (/,/.test(input)) {
+      value = input.split(',');
+    } else {
+      value = input;
+    }
+
+    if (typeof value === 'string') {
+      newTab(input);
+    } else if (Array.isArray(value)) {
+      value.forEach(newTab);
+    }
   }
 
-  if (typeof value === 'string') {
-    newTab(input);
-  } else if (Array.isArray(value)) {
-    value.forEach(newTab);
-  }
-}
+  // Called when the user clicks on the browser action.
+  chrome.browserAction.onClicked.addListener(openJIRAinNewTabs);
 
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(openJIRAinNewTabs);
+}));
